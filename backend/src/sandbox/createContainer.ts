4ba040be38e3bfe0cb4b7e85d2187e9181b4e9f2
the-akemsingh <singhakem03@gmail.com>
2026-08-15 22:01:11 +0530

    binding the sandbox container to random host port

--- backend/src/sandbox/createContainer.ts
+++ backend/src/sandbox/createContainer.ts
@@ -4,15 +4,24 @@ const docker = new Docker();
 
 //this just creates a container using node:22 image
 // project files will be built on top of this/inside this using respective templates : node/next
-async function createSandboxContainer(projectId: string) {
+async function createSandboxContainer(projectId: string, port: number) {
+    const containerPortKey = `${port}/tcp`;
     const container = await docker.createContainer({
         Image: "node:22",
         name: `sandbox-${projectId}`,
         Cmd: ["tail", "-f", "/dev/null"],
         WorkingDir: "/workspace",
         HostConfig: {
-            Memory: 512 * 1024 * 1024, //Restricts to 512 mb ram
-            NanoCpus: 1_000_000_000, // Restricts cpu usage to 1 core = 1*10^9 CPU nano-units
+            Memory: 512 * 1024 * 1024, // Restricts to 512 MB RAM
+            NanoCpus: 1_000_000_000,   // Restricts CPU usage to 1 core
+
+            // bind container port to a random host port
+            PortBindings: {
+                [containerPortKey]: [{ HostPort: "" }] // "" forces Docker to choose a free port
+            }
+        },
+        ExposedPorts: {
+            [containerPortKey]: {}
         },
     });
 
